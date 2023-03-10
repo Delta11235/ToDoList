48086cddc8f1401554aa7dce5668076352b097c3
Delta11235 <mateusz._01@wp.pl>
2023-03-10 22:32:56 +0100

    kurwi sie to

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function Dodaj() {
-    let a = document.getElementsByClassName('input').value 
-    toString.a;
-    document.getElementById("lista").innerHTML = a;
+    let a = document.getElementsByClassName('input').value;
+    //document.write(a);
+    document.getElementById("lista").innerHTML = "<h1>" + a +"</h1>";
 }
